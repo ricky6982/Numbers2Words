adec9812619472e76bd180a4a84b1ca26acd2919
RickySSD <ricky6982@gmail.com>
2015-01-30 22:00:48 -0300

    se agrego el copyright de la libreria.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- src/Rck6982/Numbers2Words/Numbers2Words.php
+++ src/Rck6982/Numbers2Words/Numbers2Words.php
@@ -1,5 +1,48 @@
 <?php
 
+/**
+ * Numbers2Words es una adaptación de la libreria Numbers_words
+ *
+ * 
+ * Numbers_words
+ * 
+ * PHP version 5
+ *  
+ * 
+ * Copyright (c) 1997-2006 The PHP Group
+ *
+ * This source file is subject to version 3.01 of the PHP license,
+ * that is bundled with this package in the file LICENSE, and is
+ * available at through the world-wide-web at
+ * http://www.php.net/license/3_01.txt
+ * If you did not receive a copy of the PHP license and are unable to
+ * obtain it through the world-wide-web, please send a note to
+ * license@php.net so we can mail you a copy immediately.
+ *
+ * Authors: Piotr Klaban <makler@man.torun.pl>
+ *
+ * @category Numbers
+ * @package  Numbers_Words
+ * @author   Piotr Klaban <makler@man.torun.pl>
+ * @license  PHP 3.01 http://www.php.net/license/3_01.txt
+ * @version  SVN: $Id$
+ * @link     http://pear.php.net/package/Numbers_Words
+ */
+
+// {{{ Numbers_Words
+
+/**
+ * The Numbers_Words class provides method to convert arabic numerals to words.
+ *
+ * @category Numbers
+ * @package  Numbers_Words
+ * @author   Piotr Klaban <makler@man.torun.pl>
+ * @license  PHP 3.01 http://www.php.net/license/3_01.txt
+ * @link     http://pear.php.net/package/Numbers_Words
+ * @since    PHP 4.2.3
+ * @access   public
+ */
+
 namespace Rck6982\Numbers2Words;
 
 use Rck6982\Numbers2Words\Numbers2WordsException;
