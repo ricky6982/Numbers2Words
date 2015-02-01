33578f7405185d53c66c1515cb2f825e893243d2
RickySSD <ricky6982@gmail.com>
2015-02-01 05:10:51 -0300

    Se agrego test unitario.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

---
+++ tests/Rck6982/Tests/Numbers2Words/Numbers2WordsTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Rck6982\Tests\Numbers2Words;
+
+use Rck6982\Numbers2Words\Numbers2Words;
+
+class Numbers2WordsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testToWords()
+    {
+        $valores = array(
+            45 => 'cuarenta y cinco',
+            20 => 'veinte',
+            458 => 'cuatrocientos cincuenta y ocho',
+            8659 => 'ocho mil seiscientos cincuenta y nueve'
+        );
+
+        $obj = new Numbers2Words();
+
+        foreach ($valores as $key => $value) {
+            $this->assertEquals($value, $obj->toWords($key));
+        }
+    }
+
+}
