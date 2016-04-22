67ae59e92ce7d3b6d53a82501783d70690046653
RickySSD <ricky6982@gmail.com>
2016-04-21 22:29:07 -0300

    Actualizando Tests.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- tests/Rck6982/Tests/Numbers2Words/Numbers2WordsTest.php
+++ tests/Rck6982/Tests/Numbers2Words/Numbers2WordsTest.php
@@ -11,7 +11,8 @@ class Numbers2WordsTest extends \PHPUnit_Framework_TestCase
             45 => 'cuarenta y cinco',
             20 => 'veinte',
             458 => 'cuatrocientos cincuenta y ocho',
-            8659 => 'ocho mil seiscientos cincuenta y nueve'
+            8659 => 'ocho mil seiscientos cincuenta y nueve',
+            32126865 => 'treinta y dos millones ciento veintiseis mil ochocientos sesenta y cinco',
         );
 
         $obj = new Numbers2Words();
